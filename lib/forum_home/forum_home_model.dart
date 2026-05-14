b379c9cac3149a07aeb1b173da435dd975dede1a
Eya Ben Hassine <eyabenhassine003@gmail.com>
2026-05-14 03:42:56 +0100

    Fix forum home interactions

--- lib/forum_home/forum_home_model.dart
+++ lib/forum_home/forum_home_model.dart
@@ -1,12 +1,20 @@
 import '/flutter_flow/flutter_flow_util.dart';
-import '/index.dart';
 import 'forum_home_widget.dart' show ForumHomeWidget;
 import 'package:flutter/material.dart';
 
 class ForumHomeModel extends FlutterFlowModel<ForumHomeWidget> {
+  FocusNode? searchFocusNode;
+  TextEditingController? searchTextController;
+  String? Function(BuildContext, String?)? searchTextControllerValidator;
+
+  String selectedCategory = 'Tous';
+
   @override
   void initState(BuildContext context) {}
 
   @override
-  void dispose() {}
+  void dispose() {
+    searchFocusNode?.dispose();
+    searchTextController?.dispose();
+  }
 }
